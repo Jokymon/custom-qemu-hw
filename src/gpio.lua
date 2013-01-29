11d2a9e604d21c3a269d7641e47974fbe6c33e88
Mario Konrad <mario.konrad@bbv.ch>
2013-01-29 06:46:37 -0800

    A bit more documenation and function to the LUA demo script.

--- src/gpio.lua
+++ src/gpio.lua
@@ -1,7 +1,14 @@
 
+-- very basic (without any useful function) demonstration of a script
+-- to be used by the special GPIO device 'pl061bbv'.
+
 function gpio_set(id, val)
 	print("gpio " .. id .. " set to " .. val)
 end
 
-print("Hello World")
+function gpio_get(id)
+	return 0
+end
+
+print("gpio init")
 
